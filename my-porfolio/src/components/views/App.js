cf6a7a682fbbaf98e3fe3b71e260e54f50706c69
buildingsoup <mns0327@naver.com>
2021-12-04 00:01:42 +0900

    feat: add Home Component

--- my-porfolio/src/components/views/App.js
+++ my-porfolio/src/components/views/App.js
@@ -1,9 +1,11 @@
+import Home from './Home/Home';
 import NavBar from './NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
---
+++ my-porfolio/src/components/views/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Home.scss';
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="moto">"SLOWLY, STEADILY, BUT ALL THE WAY!"</div>
+      <div className="intro">
+        <div className="profile">
+          <img src="img/profileImg.jpg" />
+        </div>
+        <div className="explain">
+          안녕하세요.
+          <br />
+          풀스택 개발자 박민석 입니다.
+          <br />
+          천천히, 꾸준히 성장하는 개발자가 되겠습니다.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
